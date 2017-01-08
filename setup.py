3619d1c5052428bbe2d3a54400635a112d3b3103
Fabio Codiglioni <fabiocody@icloud.com>
2017-01-08 21:18:03 +0100

    Version up for pypi deploy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 
 setup(
     name='pynetatmo',
-    version='0.0.19',
+    version='0.0.20',
     description='Netatmo API wrapper written in Python',
     long_description=long_description,
     url='https://github.com/fabiocody/PyNetatmo.git',
